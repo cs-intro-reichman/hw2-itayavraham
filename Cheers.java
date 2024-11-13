dd7a10e07190ffa4cb1d4469eafc28ee321b1398
unknown <itay.avraham01@post.runi.ac.il>
2024-11-13 13:00:01 +0200

    pushing Cheers for testing

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,25 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+                String quote = args[0].toUpperCase(), anCharacters = "AEFHILMNORSX";
+                int numOfCheers = Integer.parseInt(args[1]);
+                boolean flag = false;                   
+                for (int i = 0; i < quote.length(); i++) {
+                        for (int j = 0; j < anCharacters.length(); j++) {
+                                if (quote.indexOf(anCharacters.charAt(j), i) == i) {
+                                        System.out.println("Give me an " + quote.charAt(i) + ": " + quote.charAt(i) +"!");
+                                        flag = true;
+                                        break;
+                                }  
+                        }
+                        if (flag)
+                        flag = false;
+                        else
+                        System.out.println("Give me a " + quote.charAt(i) + ": " + quote.charAt(i) +"!");            
+                }
+                System.out.println("What does that spell?");
+                for (int i = 0; i < numOfCheers; i++) {
+                        System.out.println(quote + "!!!");
+                }
         }
 }
